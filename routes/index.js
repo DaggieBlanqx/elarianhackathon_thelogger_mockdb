1d5562bd290005cd6f6710c1a266350d728a9a40
daggieblanqx <daggieblanqx@gmail.com>
2021-11-13 16:31:37 +0300

    Updated Code

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,36 @@ router.get('/', function (req, res, next) {
     res.json(data);
 });
 
+router.get('/id/:id', function (req, res) {
+    const id = req.params.id;
+    if (!id) {
+        res.status(400).json({
+            error: 'id is required',
+        });
+    }
+
+    var user = findById(id);
+
+    if (user) {
+        res.json({
+            status: 'success',
+            data: user,
+        });
+    } else {
+        res.json({
+            error: 'No user like this',
+        });
+    }
+});
+
 router.get('/firstname/:firstname', function (req, res) {
     var name = capitalizeText(req.params.firstname);
+
+    if (!name) {
+        res.status(400).json({
+            error: 'firstname is required',
+        });
+    }
     var user = findByFirstname(name);
 
     if (user) {
@@ -34,6 +62,11 @@ router.get('/firstname/:firstname', function (req, res) {
 
 router.get('/lastname/:lastname', function (req, res) {
     var name = capitalizeText(req.params.lastname);
+    if (!name) {
+        res.status(400).json({
+            error: 'lastname is required',
+        });
+    }
     var user = findByLastname(name);
 
     if (user) {
@@ -50,6 +83,11 @@ router.get('/lastname/:lastname', function (req, res) {
 
 router.get('/email/:email', function (req, res) {
     var email = req.params.email;
+    if (!email) {
+        res.status(400).json({
+            error: 'email is required',
+        });
+    }
     var user = findByEmail(email);
 
     if (user) {
@@ -66,6 +104,11 @@ router.get('/email/:email', function (req, res) {
 
 router.get('/phone/:phone', function (req, res) {
     var phone = req.params.phone;
+    if (!phone) {
+        res.status(400).json({
+            error: 'phone is required',
+        });
+    }
     var user = findByPhone(phone);
 
     if (user) {
