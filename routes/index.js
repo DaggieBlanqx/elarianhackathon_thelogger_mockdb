a901d2c4971f0e00652a369259bd670396166b4e
daggieblanqx <daggieblanqx@gmail.com>
2021-11-13 16:26:11 +0300

    Updated Code

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,53 @@ router.get('/firstname/:firstname', function (req, res) {
         });
     }
 });
+
+router.get('/lastname/:lastname', function (req, res) {
+    var name = capitalizeText(req.params.lastname);
+    var user = findByLastname(name);
+
+    if (user) {
+        res.json({
+            status: 'success',
+            data: user,
+        });
+    } else {
+        res.json({
+            error: 'No user like this',
+        });
+    }
+});
+
+router.get('/email/:email', function (req, res) {
+    var email = req.params.email;
+    var user = findByEmail(email);
+
+    if (user) {
+        res.json({
+            status: 'success',
+            data: user,
+        });
+    } else {
+        res.json({
+            error: 'No user like this',
+        });
+    }
+});
+
+router.get('/phone/:phone', function (req, res) {
+    var phone = req.params.phone;
+    var user = findByPhone(phone);
+
+    if (user) {
+        res.json({
+            status: 'success',
+            data: user,
+        });
+    } else {
+        res.json({
+            error: 'No user like this',
+        });
+    }
+});
+
 module.exports = router;
